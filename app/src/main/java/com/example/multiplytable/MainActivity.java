6b3092d0fa8eedf45ca9c57fd092199d6a0bcb92
saad <saadfadi831@gmail.com>
2024-10-08 15:44:25 +0500

    hello world

--- app/src/main/java/com/example/multiplytable/MainActivity.java
+++ app/src/main/java/com/example/multiplytable/MainActivity.java
@@ -47,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 multiTable();
             }
+            // hello word
         });
     }
     public void multiTable(){
